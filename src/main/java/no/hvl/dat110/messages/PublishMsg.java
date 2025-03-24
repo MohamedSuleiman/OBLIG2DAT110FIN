a2a79dc89295f8c2e4367ced3883c939142495fa
MohamedSuleiman <600801@stud.hvl.no>
2025-03-24 15:12:02 +0100

    Complete task 1

--- src/main/java/no/hvl/dat110/messages/PublishMsg.java
+++ src/main/java/no/hvl/dat110/messages/PublishMsg.java
@@ -4,10 +4,27 @@ import no.hvl.dat110.common.TODO;
 
 public class PublishMsg extends Message {
 	
-	// message sent from client to create publish a message on a topic 
+	// message sent from client to create publish a message on a topic
+	private String topic;
+	private String message;
 
 	public PublishMsg(String user, String topic, String message) {
+		super(MessageType.PUBLISH,user);
+		this.topic = topic;
+		this.message = message;
 
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
 	// TODO:
@@ -17,7 +34,14 @@ public class PublishMsg extends Message {
 	// as described in the project text
 	
 	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "PublishMsg{" +
+				"topic='" + topic + '\'' +
+				", message='" + message + '\'' +
+				'}';
 	}
 }
